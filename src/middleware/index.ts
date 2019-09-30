ff9331a41f7a9488ccdc0c440762936f3df73ac5
claukers <claukers@melia>
2019-09-29 21:57:47 -0300

    bodyparser now uses FeatureToggle

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,18 +1,20 @@
 import * as bodyParser from "body-parser";
-import { Util } from "miqro-core";
+import { FeatureToggle, Util } from "miqro-core";
 import * as morgan from "morgan";
 
 export const setupMiddleware = async (app, logger) => {
   app.disable("x-powered-by");
   app.use(morgan("combined", { stream: logger.stream }));
-  Util.checkEnvVariables(["BODYPARSER_INFLATE", "BODYPARSER_LIMIT", "BODYPARSER_STRICT", "BODYPARSER_TYPE"]);
-  app.use(bodyParser.json({
-    inflate: process.env.BODYPARSER_INFLATE === "true" ? true : false,
-    limit: process.env.BODYPARSER_LIMIT,
-    // reviver: undefined,
-    strict: process.env.BODYPARSER_STRICT === "true" ? true : false,
-    type: process.env.BODYPARSER_TYPE
-    // verify: undefined
-  }));
+  if (FeatureToggle.isFeatureEnabled("bodyparser")) {
+    Util.checkEnvVariables(["BODYPARSER_INFLATE", "BODYPARSER_LIMIT", "BODYPARSER_STRICT", "BODYPARSER_TYPE"]);
+    app.use(bodyParser.json({
+      inflate: process.env.BODYPARSER_INFLATE === "true" ? true : false,
+      limit: process.env.BODYPARSER_LIMIT,
+      // reviver: undefined,
+      strict: process.env.BODYPARSER_STRICT === "true" ? true : false,
+      type: process.env.BODYPARSER_TYPE
+      // verify: undefined
+    }));
+  }
   return app;
 };
