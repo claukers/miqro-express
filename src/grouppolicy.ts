ee245c1b79d58e53d103701ae9a27813c9159aae
claukers <claukers@melia>
2019-09-30 20:41:23 -0300

    GroupPolicy utility added

---
+++ src/grouppolicy.ts
@@ -0,0 +1,51 @@
+import { ParseOptionsError, Util } from "miqro-core";
+import { IServiceArgs, ISession } from "./service/common";
+
+export type IGroupPolicy = "at_leats_one" | "all";
+
+export interface IGroupPolicyOptions {
+  groups: string[];
+  groupPolicy: IGroupPolicy;
+  name?: string;
+}
+
+export abstract class GroupPolicy {
+  public static async validateSession(session: ISession, options: IGroupPolicyOptions) {
+    const logger = Util.getLogger(`${options.name}${options.name ? "::" : ""}GroupPolicy`);
+    if (!session || !session.account || !session.username) {
+      throw new ParseOptionsError(`Invalid authentication!`);
+    }
+    let ret = false;
+    switch (options.groupPolicy) {
+      case "at_leats_one":
+        ret = false;
+        for (const group of session.groups) {
+            if (options.groups.indexOf(group) !== -1) {
+              ret = true;
+              break;
+            }
+          }
+        break;
+      case "all":
+        ret = true;
+        for (const group of options.groups) {
+          if (session.groups.indexOf(group) === -1) {
+            ret = false;
+            break;
+          }
+        }
+        break;
+      default:
+        this.logger.error(`policy [${options.groupPolicy}] not implemented!!`);
+        throw new ParseOptionsError(`policy not implemented!`);
+    }
+    if (!ret) {
+      this.logger.warn(`unauthorized token[${session.token}] with groups[${session.groups.toString()}]` +
+        ` not on correct groups [${options.groups.toString()}] with policy [${options.groupPolicy}]`);
+      throw new ParseOptionsError(`Not on the correct groups!`);
+    } else {
+      this.logger.debug(`authorized token[${session.token}] with groups[${session.groups.toString()}]` +
+        ` on correct groups [${options.groups.toString()}] with policy [${options.groupPolicy}]`);
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./route";
 export * from "./middleware";
 export * from "./service";
+export * from "./grouppolicy";
