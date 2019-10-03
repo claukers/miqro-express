efb047ce62c7128c78fb57935742e4d970268249
claukers <claukers@melia>
2019-10-03 19:28:49 -0300

    update service router args

--- src/route/service.ts
+++ src/route/service.ts
@@ -2,22 +2,22 @@ import { createAPIHandler, IServiceHandler, IServiceRouteOptions } from "./commo
 import { Route } from "./route";
 
 export class ServiceRoute extends Route {
-  public get(route: string | string[], handler: IServiceHandler) {
+  public get(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.get(route, createAPIHandler(handler, this));
   }
-  public post(route: string | string[], handler: IServiceHandler) {
+  public post(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.post(route, createAPIHandler(handler, this));
   }
-  public delete(route: string | string[], handler: IServiceHandler) {
+  public delete(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.delete(route, createAPIHandler(handler, this));
   }
-  public patch(route: string | string[], handler: IServiceHandler) {
+  public patch(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.patch(route, createAPIHandler(handler, this));
   }
-  public put(route: string | string[], handler: IServiceHandler) {
+  public put(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.put(route, createAPIHandler(handler, this));
   }
-  public use(route: string | string[], handler: IServiceHandler) {
+  public use(route: string | string[], handler: IServiceHandler | IServiceHandler[]) {
     super.use(route, createAPIHandler(handler, this));
   }
 }
