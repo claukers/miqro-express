89f8e51ecc8fd84d0ec92870d1a3e41d3698d46c
claukers <claukers@melia>
2019-10-03 02:13:37 -0300

    added createSessionHandler

--- src/route/session.ts
+++ src/route/session.ts
@@ -1,5 +1,5 @@
 import { ISession, Util } from "miqro-core";
-import { IServiceRouteOptions } from "./common";
+import { IServiceHandler, IServiceRouteOptions } from "./common";
 import { BadRequestResponse, IAPIRequest } from "./response";
 import { ServiceRoute } from "./service";
 
@@ -13,40 +13,36 @@ export interface IVerifyTokenService {
   verify(args: { token: string }): Promise<ISession>;
 }
 
+export const createSessionHandler = (authService: IVerifyTokenService, logger): IServiceHandler =>
+  async (req: IAPIRequest, res, next) => {
+    try {
+      Util.checkEnvVariables(["TOKEN_HEADER"]);
+      const token = req.headers[process.env.TOKEN_HEADER.toLowerCase()];
+      if (!token) {
+        await new BadRequestResponse(`No token provided!`).send(res);
+      } else {
+        const session: ISession = await authService.verify({ token });
+        if (!session) {
+          await new BadRequestResponse(`Fail to authenticate token!`).send(res);
+        } else {
+          req.session = session;
+          next();
+        }
+      }
+    } catch (e) {
+      logger.error(e);
+      await new BadRequestResponse(`Fail to authenticate token!`).send(res);
+    }
+  };
+
 export class SessionRoute extends ServiceRoute {
   protected authService: IVerifyTokenService;
-  protected middlewareSetupDone: boolean = false;
   constructor(options?: ISessionRouteOptions) {
     super(options);
     if (!logger) {
       logger = Util.getLogger("SessionRoute");
     }
-    Util.checkEnvVariables(["TOKEN_HEADER"]);
     this.authService = options.authService;
-    this.setupMiddleware();
-  }
-  protected setupMiddleware() {
-    if (!this.middlewareSetupDone) {
-      this.middlewareSetupDone = true;
-      this.router.use(async (req: IAPIRequest, res, next) => {
-        try {
-          const token = req.headers[process.env.TOKEN_HEADER.toLowerCase()];
-          if (!token) {
-            await new BadRequestResponse(`No token provided!`).send(res);
-          } else {
-            const session: ISession = await this.authService.verify({ token });
-            if (!session) {
-              await new BadRequestResponse(`Fail to authenticate token!`).send(res);
-            } else {
-              req.session = session;
-              next();
-            }
-          }
-        } catch (e) {
-          logger.error(e);
-          await new BadRequestResponse(`Fail to authenticate token!`).send(res);
-        }
-      });
-    }
+    this.router.use(createSessionHandler(options.authService, logger));
   }
 }
