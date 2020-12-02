d602b8af7673a42185441e1d440bae200140da75
ro <ro@localhost.localdomain>
2020-12-02 01:48:58 -0300

    grouphandler to group.ts

---
+++ src/handler/group.ts
@@ -0,0 +1,43 @@
+import {
+  ForbiddenError,
+  GroupPolicy,
+  GroupPolicyValidator,
+  Logger,
+  ParseOptionsError,
+  UnAuthorizedError,
+  Util
+} from "@miqro/core";
+import {AsyncNextCallback} from "./common";
+
+export const GroupPolicyHandler = (options: GroupPolicy, logger?: Logger): AsyncNextCallback => {
+  if (!logger) {
+    logger = Util.getLogger("GroupPolicyHandler");
+  }
+  return async (req, res, next) => {
+    try {
+      if (!req.session) {
+        next(new ParseOptionsError(`No Session!`));
+      } else {
+        const result = await GroupPolicyValidator.validate(req.session, options, logger as Logger);
+        if (result) {
+          (logger as Logger).info(`request[${req.uuid}] ` +
+            `groups [${req && req.session && req.session.groups ? req.session.groups.join(",") : ""}] validated!`);
+
+          next();
+        } else {
+          (logger as Logger).warn(`request[${req.uuid}] ` +
+            `groups [${req && req.session && req.session.groups ? req.session.groups.join(",") : ""}] fail to validate!`);
+
+          next(new UnAuthorizedError(`Invalid session. You are not permitted to do this!`));
+        }
+      }
+    } catch (e) {
+      (logger as Logger).warn(`request[${req.uuid}] message[${e.message}] stack[${e.stack}]`);
+      if (e.name && e.name !== "Error") {
+        next(e);
+      } else {
+        next(new ForbiddenError(`Invalid session. You are not permitted to do this!`));
+      }
+    }
+  };
+};
--- src/handler/index.ts
+++ src/handler/index.ts
@@ -5,6 +5,7 @@ export * from "./api-router";
 export * from "./response";
 export * from "./responses";
 export * from "./session";
+export * from "./group";
 export * from "./queryasparams";
 export * from "./validatebody";
 export * from "./test_helper";
