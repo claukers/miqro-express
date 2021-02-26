692fc5680ed1ac714d39e01745776749e60d7119
claukers <claukers@localhost>
2021-02-26 03:02:48 -0300

    fix errorhandler

--- src/handler/error.ts
+++ src/handler/error.ts
@@ -7,23 +7,30 @@ export const DefaultErrorHandler = (): ErrorHandler => {
     if (!e.name || e.name === "Error") {
       if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
         await ctx.end(ERROR_RESPONSE(`${e.message}. You are seeing this message because NODE_ENV === "development" || NODE_ENV === "test"`));
+        return false;
       }
     } else {
       switch (e.name) {
         case "MethodNotImplementedError":
           await ctx.end(NOT_FOUND(e.message));
+          break;
         case "ForbiddenError":
           await ctx.end(FORBIDDEN(e.message));
+          break;
         case "UnAuthorizedError":
           await ctx.end(UNAUTHORIZED(e.message));
+          break;
         case "ParseOptionsError":
         case "SequelizeValidationError":
         case "SequelizeEagerLoadingError":
         case "SequelizeUniqueConstraintError":
           await ctx.end(BAD_REQUEST(e.message));
+          break;
         default:
           await ctx.end(ERROR_RESPONSE(e.message));
+          break;
       }
+      return false;
     }
     ctx.logger.warn(`cannot create error response for error message[${e.message}] so not sending error response`);
   };
