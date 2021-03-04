a5b41e355c0bff33f67b38d088c3c47012eb9056
claukers <claukers@localhost>
2021-03-04 15:08:45 -0300

    status remove space

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -8,7 +8,7 @@ export const Logger = (options?: {
       const took = Date.now() - ctx.startMS;
       ctx.tookMS = took;
       const contentLength = (ctx.res as any)._contentLength;
-      const entry = options ? options.formatter(ctx) : `status [${ctx.res.statusCode}] content-length[${contentLength}] [${ctx.tookMS}]ms`;
+      const entry = options ? options.formatter(ctx) : `status[${ctx.res.statusCode}] content-length[${contentLength}] [${ctx.tookMS}]ms`;
       if (ctx.res.statusCode < 400) {
         ctx.logger.info(entry);
       } else if (ctx.res.statusCode >= 400 && ctx.res.statusCode < 500) {
