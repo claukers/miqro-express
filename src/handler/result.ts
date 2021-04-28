de265efc317b490a8c339013fda7921871458691
claukers <claukers@github>
2021-04-28 17:48:36 -0400

    restore disableAsArray on ResultParse

--- src/handler/result.ts
+++ src/handler/result.ts
@@ -1,4 +1,4 @@
-import { Context, Handler, parseOptions } from "@miqro/core";
+import { Context, Handler, parseOptions, ParseOptionsError } from "@miqro/core";
 import { inspect } from "util";
 import { ParseOptions } from "./common";
 
@@ -12,9 +12,23 @@ export const ResultParser = (options: ParseOptions): Handler =>
       const lastResult = ctx.results[ctx.results.length - 1];
       ctx.logger.debug(`parsing lastResult[%s]`, inspect(lastResult));
       try {
-        const mappedResults = parseOptions(`ctx.results[${index}]`, lastResult, options.options, options.mode, options.ignoreUndefined);
-        ctx.logger.debug(`ctx.results[%s] mapped to %s`, index, inspect(mappedResults));
-        return mappedResults;
+        if (options.disableAsArray && lastResult instanceof Array) {
+          throw new ParseOptionsError(`ctx.results cannot be an array [%s]`, inspect(lastResult));
+        }
+        if (lastResult instanceof Array && !options.disableAsArray) {
+          const newList = [];
+          ctx.logger.debug("last result is array and options.disableAsArray is not set so iterating");
+          for (const r of lastResult) {
+            const mappedResults = parseOptions(`ctx.results[${index}][..]`, r, options.options, options.mode, options.ignoreUndefined);
+            ctx.logger.debug(`ctx.results[%s] mapped to %s`, index, inspect(mappedResults));
+            newList.push(mappedResults);
+          }
+          return newList;
+        } else {
+          const mappedResults = parseOptions(`ctx.results[${index}]`, lastResult, options.options, options.mode, options.ignoreUndefined);
+          ctx.logger.debug(`ctx.results[%s] mapped to %s`, index, inspect(mappedResults));
+          return mappedResults;
+        }
       } catch (e) {
         ctx.logger.error(`error parsing lastResult[%s]`, inspect(lastResult));
         throw e;
