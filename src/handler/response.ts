62cb6ed1bb7d205bda85378aba6c2994d92f6e12
claukers <claukers@github>
2021-05-14 17:45:02 -0400

    less verbose response handler and update deps

--- src/handler/response.ts
+++ src/handler/response.ts
@@ -3,13 +3,13 @@ import { Handler, Context } from "@miqro/core";
 
 export const ResponseHandler = (): Handler =>
   async (ctx: Context) => {
-    ctx.logger.debug(`results[${inspect(ctx.results)}]`);
+    // ctx.logger.debug(`results[${inspect(ctx.results)}]`);
     if (!ctx.results || ctx.results.length === 0) {
       return undefined;
     } else {
       const lastResult = ctx.results[ctx.results.length - 1];
 
-      ctx.logger.debug(`response lastResult[${inspect(lastResult)}]`);
+      ctx.logger.debug(`response with lastResult[${inspect(lastResult)}]`);
       if (lastResult !== undefined) {
         await ctx.json(lastResult);
       } else {
