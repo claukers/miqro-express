b3255485fd8900fae1a49d0248c12bfd1a187c78
claukers <claukers@github>
2021-07-15 06:12:46 -0400

    update deps and added CORS to middleware()

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,13 +1,11 @@
-import { Handler, FeatureToggle, LoggerHandler, ReadBuffer, JSONParser, URLEncodedParser, TextParser } from "@miqro/core";
+import { CORS, Handler, FeatureToggle, LoggerHandler, ReadBuffer, JSONParser, URLEncodedParser, TextParser } from "@miqro/core";
 import { CookieParser } from "./cookie";
 import { TagResponse } from "./tag";
 import { UUIDHandler } from "./uuid";
-// import { CORSHandler } from "./cors";
 
 export * from "./tag";
 export * from "./cookie";
 export * from "./uuid";
-// export * from "./cors";
 
 export const middleware = (): Handler[] => {
   const ret: Handler[] = [];
@@ -20,9 +18,9 @@ export const middleware = (): Handler[] => {
   if (FeatureToggle.isFeatureEnabled("TAG_RESPONSE", false)) {
     ret.push(TagResponse());
   }
-  /*if (FeatureToggle.isFeatureEnabled("CORS", true)) {
-    ret.push(CORSHandler());
-  }*/
+  if (FeatureToggle.isFeatureEnabled("CORS", false)) {
+    ret.push(CORS());
+  }
   if (FeatureToggle.isFeatureEnabled("READ_BUFFER", true)) {
     ret.push(ReadBuffer());
   }
